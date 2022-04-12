564276d7d83a1667a3b27edd9d069b5bc34aeef8
arthurariza <tucaariza@gmail.com>
2022-04-12 00:48:11 -0300

    WIP

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,5 +1,17 @@
+require String
+require Integer
+
 defmodule ListFilter do
-  def hello do
-    :world
+  def call(list) do
+    Enum.count(remove_strings(list))
+  end
+
+  defp remove_strings(list) do
+    Enum.flat_map(list, fn x ->
+      case Integer.parse(x) do
+        {int, _rest} -> [int]
+        :error -> []
+      end
+    end)
   end
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,7 +1,53 @@
 defmodule ListFilterTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "ListFilter.call/1" do
+    test "when given an empty list" do
+      empty_list = []
+
+      response = ListFilter.call(empty_list)
+
+      assert response == 0
+    end
+
+    test "when given a list with numbers" do
+      numbers_list = [1, 2, 3, 4, 6]
+
+      response = ListFilter.call(numbers_list)
+
+      assert response == 2
+    end
+
+    test "when given a list with negative numbers" do
+      negative_list = [-1, -2, -3, -4, 6]
+
+      response = ListFilter.call(negative_list)
+
+      assert response == 2
+    end
+
+    test "when given a list with strings" do
+      strings_list = ["1", "2", "3", "4", "6"]
+
+      response = ListFilter.call(strings_list)
+
+      assert response == 2
+    end
+
+    test "when given a list with numbers and strings" do
+      mixed_list = ["1", "2", 3, "4", "6", "a", "b", "c"]
+
+      response = ListFilter.call(mixed_list)
+
+      assert response == 2
+    end
   end
 end
+
+# Filtre todos os elementos numéricos da lista e retorne quantos números ímpares
+# existem nessa lista.
+
+# ListFilter.call(["1", "3", "6", "43", "banana", "6", "abc"])
+# > 3
+
+# Enum.count([1,2,3, "3", "5"], fn x -> Integer.is_odd(String.to_integer(x))  end)
